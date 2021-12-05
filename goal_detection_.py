fa49ad8827a54ccda35891168c6d67ab685c8bbd
Akhilesh Bellathur <abellath@usc.edu>
2021-12-04 18:55:08 -0800

    Rectangle detection works :)

--- goal_detection_.py
+++ goal_detection_.py
@@ -13,23 +13,30 @@ def nothing(x):
 
 cv2.namedWindow("Trackbars")
 cv2.createTrackbar("L-H", "Trackbars", 0, 180, nothing)
-# cv2.createTrackbar("L-S", "Trackbars", 0, 255, nothing)
-# cv2.createTrackbar("L-V", "Trackbars", 0, 255, nothing)
-# cv2.createTrackbar("U-H", "Trackbars", 0, 180, nothing)
-# cv2.createTrackbar("U-S", "Trackbars", 0, 255, nothing)
-# cv2.createTrackbar("U-V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L-S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L-V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("U-H", "Trackbars", 180, 180, nothing)
+cv2.createTrackbar("U-S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U-V", "Trackbars", 146, 255, nothing)
 
 while True:
     ret, frame = cam.read()
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
     l_h = cv2.getTrackbarPos("L-H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L-S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L-V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U-H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U-S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U-V", "Trackbars")
 
     
     ## Color detection 
-    low_red = np.array([0, 13, 155])
-    high_red = np.array([180, 255, 240])
+    low_red = np.array([l_h, l_s, l_v])
+    high_red = np.array([u_h, u_s, u_v])
+    # print(f"{l_h}, {l_s}, {l_v}, {u_h}, {u_s}, {u_v}")
     mask = cv2.inRange(hsv_frame, low_red, high_red)
+
     kernel = np.ones((5, 5), np.uint8)
     mask = cv2.erode(mask, kernel)
 
@@ -43,6 +50,8 @@ while True:
             cv2.drawContours(frame, [approx], 0, (0,0,0), 5)
             if len(approx)==4:
                 print("ITS A RECT")
+            else:
+                print("")
 
     if not ret:
         print("failed to grab frame")
